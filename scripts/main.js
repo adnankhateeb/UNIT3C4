11feb18c916821a2bd4adb6c8889fa9b147fdc5b
adnankhateeb <adnankhateeb21@gmail.com>
2022-02-21 17:15:38 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -66,7 +66,7 @@ function appendArticle(articles, main) {
         let img = document.createElement('img');
         img.src = el.urlToImage;
 
-        let title = document.createElement('p')
+        let title = document.createElement('h1')
         title.innerText = el.title;
         title.style.fontWeight = "bold"
 
